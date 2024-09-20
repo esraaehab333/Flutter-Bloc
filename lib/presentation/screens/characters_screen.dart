bf90bfc266a74849346cc04178b097f5c0292691
esraaehab333 <esraaehabb333@gmail.com>
2024-09-20 03:17:42 +0300

    edit appbar

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -14,11 +14,84 @@ class CharactersScreen extends StatefulWidget {
 
 class _CharactersScreenState extends State<CharactersScreen> {
   late List<Charcter> allCharacters;
+  late List<Charcter> searchedForCharacter;
+  bool _isSearching = false;
+  final _searchTextController = TextEditingController();
+  Widget _buildSearchField() {
+    return TextField(
+      controller: _searchTextController,
+      cursorColor: MyColors.myGrey,
+      decoration: const InputDecoration(
+        hintText: 'Find a character...',
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: MyColors.myGrey, fontSize: 18),
+      ),
+      style: const TextStyle(color: MyColors.myGrey, fontSize: 18),
+      onChanged: (searchedCharacter) {
+        addSearchedFOrItemsToSearchedList(searchedCharacter);
+      },
+    );
+  }
+
+  void addSearchedFOrItemsToSearchedList(String searchedCharacter) {
+    searchedForCharacter = allCharacters
+        .where((character) =>
+            character.name.toLowerCase().startsWith(searchedCharacter))
+        .toList();
+    setState(() {});
+  }
+
+  List<Widget> _buildAppBarActions() {
+    if (_isSearching) {
+      return [
+        IconButton(
+          onPressed: () {
+            _clearSearch();
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.clear, color: MyColors.myGrey),
+        ),
+      ];
+    } else {
+      return [
+        IconButton(
+          onPressed: _startSearch,
+          icon: const Icon(
+            Icons.search,
+            color: MyColors.myGrey,
+          ),
+        ),
+      ];
+    }
+  }
+
+  void _startSearch() {
+    ModalRoute.of(context)!
+        .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching));
+
+    setState(() {
+      _isSearching = true;
+    });
+  }
+
+  void _stopSearching() {
+    _clearSearch();
+
+    setState(() {
+      _isSearching = false;
+    });
+  }
+
+  void _clearSearch() {
+    setState(() {
+      _searchTextController.clear();
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    allCharacters =
-        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+    BlocProvider.of<CharactersCubit>(context).getAllCharacters();
     // ui need data here i get the bloc
   }
 
@@ -67,24 +140,61 @@ class _CharactersScreenState extends State<CharactersScreen> {
         shrinkWrap: true,
         padding: EdgeInsets.zero,
         physics: const ClampingScrollPhysics(),
-        itemCount: allCharacters.length,
+        itemCount: _searchTextController.text.isEmpty
+          ? allCharacters.length
+          : searchedForCharacter.length,
         itemBuilder: (context, index) {
-          return  CharacterItem(character: allCharacters[index],);
+          return CharacterItem(
+             character: _searchTextController.text.isEmpty
+              ? allCharacters[index]
+              : searchedForCharacter[index],
+          );
         });
+  }
+
+  Widget _buildAppBarTitle() {
+    return const Text(
+      'Characters',
+      style: TextStyle(color: MyColors.myGrey),
+    );
+  }
+
+  Widget buildNoInternetWidget() {
+    return Center(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Can\'t connect .. check internet',
+              style: TextStyle(
+                fontSize: 22,
+                color: MyColors.myGrey,
+              ),
+            ),
+            Image.asset('assets/images/no_internet.png')
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: MyColors.myYellow,
-        title: const Text(
-          "Characters",
-          style: TextStyle(
-            color: MyColors.myGrey,
-          ),
-        ),
-      ),
+          backgroundColor: MyColors.myYellow,
+          leading: _isSearching
+              ? const BackButton(
+                  color: MyColors.myGrey,
+                )
+              : Container(),
+          actions: _buildAppBarActions(),
+          title: _isSearching ? _buildSearchField() : _buildAppBarTitle()),
       body: buildBlocWidget(),
     );
   }
